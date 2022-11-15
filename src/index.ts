f76dd93046e4fc5ef9736e2a16340095b67cc23d
Pooya Parsa <pooya@pi0.io>
2022-11-15 01:01:35 +0100

    refactor: update repository

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { existsSync, readFileSync, writeFileSync } from 'fs'
-import { resolve } from 'path'
-import { homedir } from 'os'
-import destr from 'destr'
-import flat from 'flat'
-import defu from 'defu'
+import { existsSync, readFileSync, writeFileSync } from "node:fs";
+import { resolve } from "node:path";
+import { homedir } from "node:os";
+import destr from "destr";
+import flat from "flat";
+import defu from "defu";
 
-const RE_KEY_VAL = /^\s*([^=\s]+)\s*=\s*(.*)?\s*$/
-const RE_LINES = /\n|\r|\r\n/
+const RE_KEY_VAL = /^\s*([^\s=]+)\s*=\s*(.*)?\s*$/;
+const RE_LINES = /\n|\r|\r\n/;
 
 type RC = Record<string, any>
 
@@ -17,97 +17,98 @@ interface RCOptions {
 }
 
 export const defaults: RCOptions = {
-  name: '.conf',
+  name: ".conf",
   dir: process.cwd(),
   flat: false
-}
+};
 
 function withDefaults (options?: RCOptions | string): RCOptions {
-  if (typeof options === 'string') {
-    options = { name: options }
+  if (typeof options === "string") {
+    options = { name: options };
   }
-  return { ...defaults, ...options }
+  return { ...defaults, ...options };
 }
 
 export function parse (contents: string, options: RCOptions = {}): RC {
-  const config: RC = {}
+  const config: RC = {};
 
-  const lines = contents.split(RE_LINES)
+  const lines = contents.split(RE_LINES);
 
   for (const line of lines) {
-    const match = line.match(RE_KEY_VAL)
-    if (!match) { continue }
+    const match = line.match(RE_KEY_VAL);
+    if (!match) { continue; }
 
     // Key
-    const key = match[1]
+    const key = match[1];
 
-    if (!key || key === '__proto__' || key === 'constructor') {
-      continue
+    if (!key || key === "__proto__" || key === "constructor") {
+      continue;
     }
 
-    const val = destr(match[2].trim() /* val */)
+    const value = destr(match[2].trim() /* val */);
 
-    if (key.endsWith('[]')) {
-      const nkey = key.substr(0, key.length - 2)
-      config[nkey] = (config[nkey] || []).concat(val)
-      continue
+    if (key.endsWith("[]")) {
+      const nkey = key.slice(0, Math.max(0, key.length - 2));
+      // eslint-disable-next-line unicorn/prefer-spread
+      config[nkey] = (config[nkey] || []).concat(value);
+      continue;
     }
 
-    config[key] = val
+    config[key] = value;
   }
 
-  return options.flat ? config : flat.unflatten(config, { overwrite: true })
+  return options.flat ? config : flat.unflatten(config, { overwrite: true });
 }
 
 export function parseFile (path: string, options?: RCOptions): RC {
   if (!existsSync(path)) {
-    return {}
+    return {};
   }
-  return parse(readFileSync(path, 'utf-8'), options)
+  return parse(readFileSync(path, "utf8"), options);
 }
 
 export function read (options?: RCOptions| string): RC {
-  options = withDefaults(options)
-  return parseFile(resolve(options.dir!, options.name!), options)
+  options = withDefaults(options);
+  return parseFile(resolve(options.dir!, options.name!), options);
 }
 
 export function readUser (options?: RCOptions | string): RC {
-  options = withDefaults(options)
-  options.dir = process.env.XDG_CONFIG_HOME || homedir()
-  return read(options)
+  options = withDefaults(options);
+  options.dir = process.env.XDG_CONFIG_HOME || homedir();
+  return read(options);
 }
 
 export function serialize (config: RC): string {
   return Object.entries(flat.flatten<RC, RC>(config))
-    .map(([key, val]) => `${key}=${typeof val === 'string' ? val : JSON.stringify(val)}`)
-    .join('\n')
+    .map(([key, value]) => `${key}=${typeof value === "string" ? value : JSON.stringify(value)}`)
+    .join("\n");
 }
 
 export function write (config: RC, options?: RCOptions | string) {
-  options = withDefaults(options)
+  options = withDefaults(options);
   writeFileSync(resolve(options.dir!, options.name!), serialize(config), {
-    encoding: 'utf-8'
-  })
+    encoding: "utf8"
+  });
 }
 
 export function writeUser (config: RC, options?: RCOptions | string) {
-  options = withDefaults(options)
-  options.dir = process.env.XDG_CONFIG_HOME || homedir()
-  write(config, options)
+  options = withDefaults(options);
+  options.dir = process.env.XDG_CONFIG_HOME || homedir();
+  write(config, options);
 }
 
 export function update (config: RC, options?: RCOptions | string): RC {
-  options = withDefaults(options)
+  options = withDefaults(options);
   if (!options.flat) {
-    config = flat.unflatten(config, { overwrite: true })
+    config = flat.unflatten(config, { overwrite: true });
   }
-  const newConfig = defu(config, read(options))
-  write(newConfig, options)
-  return newConfig
+  const newConfig = defu(config, read(options));
+  write(newConfig, options);
+  return newConfig;
 }
 
 export function updateUser (config: RC, options?: RCOptions | string): RC {
-  options = withDefaults(options)
-  options.dir = process.env.XDG_CONFIG_HOME || homedir()
-  return update(config, options)
+  options = withDefaults(options);
+  options.dir = process.env.XDG_CONFIG_HOME || homedir();
+  return update(config, options);
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,66 +1,67 @@
-import { describe, test, expect } from 'vitest'
-import { write, read, readUser, parse, update, updateUser, writeUser } from '../src'
+import { describe, test, expect } from "vitest";
+import { write, read, readUser, parse, update, updateUser, writeUser } from "../src";
 
-process.env.XDG_CONFIG_HOME = __dirname
+// eslint-disable-next-line unicorn/prefer-module
+process.env.XDG_CONFIG_HOME = __dirname;
 
 const config = {
   db: {
-    username: 'db username',
-    password: 'db pass',
+    username: "db username",
+    password: "db pass",
     enabled: false
   }
-}
+};
 
-describe('rc', () => {
-  test('Write config', () => {
-    write(config)
-    expect(read()).toMatchObject(config)
-  })
+describe("rc", () => {
+  test("Write config", () => {
+    write(config);
+    expect(read()).toMatchObject(config);
+  });
 
-  test('Write config (user)', () => {
-    writeUser(config)
-    expect(readUser()).toMatchObject(config)
-  })
+  test("Write config (user)", () => {
+    writeUser(config);
+    expect(readUser()).toMatchObject(config);
+  });
 
-  test('Read config', () => {
-    expect(read('.conf')).toMatchObject(config)
-  })
+  test("Read config", () => {
+    expect(read(".conf")).toMatchObject(config);
+  });
 
-  test('Update user config', () => {
-    updateUser({ 'db.password': '"123"' })
-    expect(readUser().db.password).toBe('123')
-  })
+  test("Update user config", () => {
+    updateUser({ "db.password": "\"123\"" });
+    expect(readUser().db.password).toBe("123");
+  });
 
-  test('Parse ignore invalid lines', () => {
+  test("Parse ignore invalid lines", () => {
     expect(parse(`
       foo=bar
       __proto__=no
       # test
       bar = baz
     `)).toMatchObject({
-      foo: 'bar',
-      bar: 'baz'
-    })
-  })
+      foo: "bar",
+      bar: "baz"
+    });
+  });
 
-  test('Ignore non-existent', () => {
-    expect(read({ name: '.404' })).toMatchObject({})
-  })
+  test("Ignore non-existent", () => {
+    expect(read({ name: ".404" })).toMatchObject({});
+  });
 
-  test('Flat mode', () => {
-    const obj = { x: 1, 'x.y': 2 }
-    update(obj, { flat: true, name: '.conf2' })
-    expect(read({ flat: true, name: '.conf2' })).toMatchObject(obj)
-  })
+  test("Flat mode", () => {
+    const object = { x: 1, "x.y": 2 };
+    update(object, { flat: true, name: ".conf2" });
+    expect(read({ flat: true, name: ".conf2" })).toMatchObject(object);
+  });
 
-  test('Parse indexless arrays', () => {
+  test("Parse indexless arrays", () => {
     expect(parse(`
       x.foo[]=A
       x.foo[]=B
     `)).toMatchObject({
       x: {
-        foo: ['A', 'B']
+        foo: ["A", "B"]
       }
-    })
-  })
-})
+    });
+  });
+});
