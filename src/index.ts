35de0d3160130709a1431682a0b5f176ddcbd932
Max <maximogarciamtnez@gmail.com>
2026-02-06 12:47:28 +0100

    docs: add jsdocs to exported functions (#124)

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,28 @@ const RE_LINES = /\n|\r|\r\n/;
 export type RC = Record<string, any>;
 
 export interface RCOptions {
+  /**
+   * The name of the configuration file.
+   * @optional
+   */
   name?: string;
+
+  /**
+   * The directory where the configuration file is or should be written.
+   * @optional
+   */
   dir?: string;
+
+  /**
+   * Specifies whether the configuration should be treated as a flat object.
+   * @optional
+   */
   flat?: boolean;
 }
 
+/**
+ * The default options for the configuration file.
+ */
 export const defaults: RCOptions = {
   name: ".conf",
   dir: process.cwd(),
@@ -62,44 +79,93 @@ export function parse<T extends RC = RC>(contents: string, options: RCOptions = 
   return options.flat ? (config as T) : unflatten(config, { overwrite: true });
 }
 
-export function parseFile<T extends RC = RC>(path: string, options?: RCOptions): T {
+/**
+ * Parses a configuration string into an object.
+ * @param {string} contents - The configuration data as a raw string.
+ * @param {RCOptions} [options={}] - Options to control the parsing behaviour. See {@link RCOptions}.
+ * @returns {RC} - The parsed configuration object. See {@link RC}.
+ */
+export function parseFile<T extends RC = RC>(
+  path: string,
+  options?: RCOptions,
+): T {
   if (!existsSync(path)) {
     return {} as T;
   }
   return parse(readFileSync(path, "utf8"), options);
 }
 
+/**
+ * Reads a configuration file from a default or specified location and parses its contents.
+ * @param {RCOptions|string} [options] - Options for reading the configuration file, or the name of the configuration file. See {@link RCOptions}.
+ * @returns {RC} - The parsed configuration object. See {@link RC}.
+ */
 export function read<T extends RC = RC>(options?: RCOptions | string): T {
   options = withDefaults(options);
   return parseFile(resolve(options.dir!, options.name!), options);
 }
 
+/**
+ * Reads a custom configuration file from a default or specified location and parses its contents.
+ * @param {RCOptions|string} [options] - Options for reading the configuration file, or the name of the configuration file. See {@link RCOptions}.
+ * @returns {RC} - The parsed configuration object.
+ */
 export function readUser<T extends RC = RC>(options?: RCOptions | string): T {
   options = withDefaults(options);
   options.dir = process.env.XDG_CONFIG_HOME || homedir();
   return read(options);
 }
 
+/**
+ * Serialises a configuration object to a string format.
+ * @param {RC} config - The configuration object to serialise. See {@link RC}.
+ * @returns {string} - The serialised configuration string.
+ */
 export function serialize<T extends RC = RC>(config: T): string {
   return Object.entries(flatten<RC, RC>(config))
     .map(([key, value]) => `${key}=${JSON.stringify(value)}`)
     .join("\n");
 }
 
-export function write<T extends RC = RC>(config: T, options?: RCOptions | string) {
+/**
+ * Writes a configuration object to a file in a default or specified location.
+ * @param {RC} config - The configuration object to write. See {@link RC}.
+ * @param {RCOptions|string} [options] - Options for writing the configuration file, or the name of the configuration file. See {@link RCOptions}.
+ */
+export function write<T extends RC = RC>(
+  config: T,
+  options?: RCOptions | string,
+) {
   options = withDefaults(options);
   writeFileSync(resolve(options.dir!, options.name!), serialize(config), {
     encoding: "utf8",
   });
 }
 
-export function writeUser<T extends RC = RC>(config: T, options?: RCOptions | string) {
+/**
+ * Writes a custom configuration object to a file in a default or specified location.
+ * @param {RC} config - The configuration object to write. See {@link RC}.
+ * @param {RCOptions|string} [options] - Options for writing the configuration file, or the name of the configuration file. See {@link RCOptions}.
+ */
+export function writeUser<T extends RC = RC>(
+  config: T,
+  options?: RCOptions | string,
+) {
   options = withDefaults(options);
   options.dir = process.env.XDG_CONFIG_HOME || homedir();
   write(config, options);
 }
 
-export function update<T extends RC = RC>(config: T, options?: RCOptions | string): T {
+/**
+ * Updates an existing configuration object by merging it with the contents of a configuration file and writing the result.
+ * @param {RC} config - The configuration object to update. See {@link RC}.
+ * @param {RCOptions|string} [options] - Options for updating the configuration file, or the name of the configuration file. See {@link RCOptions}.
+ * @returns {RC} - The updated configuration object. See {@link RC}.
+ */
+export function update<T extends RC = RC>(
+  config: T,
+  options?: RCOptions | string,
+): T {
   options = withDefaults(options);
   if (!options.flat) {
     config = unflatten(config, { overwrite: true });
@@ -109,7 +175,16 @@ export function update<T extends RC = RC>(config: T, options?: RCOptions | strin
   return newConfig as T;
 }
 
-export function updateUser<T extends RC = RC>(config: T, options?: RCOptions | string): T {
+/**
+ * Updates a custom configuration object by merging it with the contents of a configuration file in a default location and writing the result.
+ * @param {RC} config - The configuration object to update. See {@link RC}.
+ * @param {RCOptions|string} [options] - Options for updating the configuration file, or the name of the configuration file. See {@link RCOptions}.
+ * @returns {RC} - The updated configuration object. See {@link RC}.
+ */
+export function updateUser<T extends RC = RC>(
+  config: T,
+  options?: RCOptions | string,
+): T {
   options = withDefaults(options);
   options.dir = process.env.XDG_CONFIG_HOME || homedir();
   return update(config, options);
